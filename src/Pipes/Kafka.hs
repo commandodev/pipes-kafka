72acca06b1a84d408a8dac63de101bf5e143cb69
Ben Ford <ben@perurbis.com>
2015-04-23 20:05:13 +0100

    WIP

--- src/Pipes/Kafka.hs
+++ src/Pipes/Kafka.hs
@@ -46,3 +46,58 @@ kafkaSink co partition = PS.bracket connect release publish
     publish (snd -> t) = forever $ do
       m <- P.await
       P.liftIO $ HK.produceMessage t partition $ HK.KafkaProduceMessage m
+{-
+
+withKafkaConsumerSource
+
+:: ConfigOverrides
+config overrides for kafka. See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md. Use an empty list if you don't care.
+-> ConfigOverrides
+config overrides for topic. See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md. Use an empty list if you don't care.
+-> String
+broker string, e.g. localhost:9092
+-> String
+topic name
+-> Int
+partition to consume from. Locked until the function returns.
+-> KafkaOffset
+where to begin consuming in the partition.
+-> (Kafka -> KafkaTopic -> IO a)
+your cod, fed with Kafka and KafkaTopic instances for subsequent interaction.
+-> IO a
+-}
+
+{-
+  bracket
+    (do
+      kafka <- newKafka RdKafkaConsumer configOverrides
+      addBrokers kafka brokerString
+      topic <- newKafkaTopic kafka tName topicConfigOverrides
+      startConsuming topic partition offset
+      return (kafka, topic)
+    )
+    (\(_, topic) -> stopConsuming topic partition)
+    (\(k, t) -> cb k t)
+-}
+
+
+
+kafkaSource
+  :: (MonadSafe m)
+  => ConnectOpts
+  -> Int
+  -> Offset
+  -> Producer HK.KafkaMessage (SafeT m) ()
+kafkaSource co partition offset = PS.bracket connect release consume
+  where
+    connect = liftIO $ do
+      kafka <- HK.newKafka HK.RdKafkaConsumer (co ^. kafkaOverrides)
+      HK.addBrokers kafka (co ^. hostString)
+      topic <- HK.newKafkaTopic kafka (co ^. topicName) (co ^. topicOverrides)
+      HK.startConsuming topic partition offset
+      return (kafka, topic)
+    release (kafka, topic) = liftIO $ HK.stopConsuming topic kafka
+    consume (snd -> t) = forever $ do
+      m <- P.liftIO $ HK.consumeMessage _ --t partition $ HK.KafkaProduceMessage m
+      P.yield m
+
