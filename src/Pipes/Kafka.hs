c7e3e26b647e3e44e98e60cccf6c850822a93005
Ben Ford <ben@perurbis.com>
2015-04-23 20:04:41 +0100

    Add a publisher/consumer

--- src/Pipes/Kafka.hs
+++ src/Pipes/Kafka.hs
@@ -1,25 +1,29 @@
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE ViewPatterns        #-}
 module Pipes.Kafka where
 
-import Control.Lens
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import qualified Data.ByteString as BS
-import           Haskakafka      (KafkaProducePartition (..), KafkaTopic)
-import qualified Haskakafka      as HK
-import qualified Haskakafka.InternalRdKafkaEnum      as HK
-import qualified Haskakafka.InternalSetup      as HK
-import           Pipes           (Consumer, Producer, runEffect)
-import qualified Pipes           as P
-import qualified Pipes.Prelude           as P
-import           Pipes.Safe      (MonadSafe, SafeT, MonadCatch)
-import qualified Pipes.Safe      as PS
+import           Control.Lens
+import Control.Monad (forever)
+import           Control.Monad.IO.Class         (liftIO)
+import qualified Data.ByteString                as BS
+import           Haskakafka                     (KafkaProducePartition (..),
+                                                 KafkaTopic)
+import qualified Haskakafka                     as HK
+import qualified Haskakafka.InternalRdKafkaEnum as HK
+import qualified Haskakafka.InternalSetup       as HK
+import           Pipes                          (Consumer, Producer, runEffect)
+import qualified Pipes                          as P
+import qualified Pipes.Prelude                  as P
+import           Pipes.Safe                     (MonadCatch, MonadSafe, SafeT)
+import qualified Pipes.Safe                     as PS
 
 type Topic = String
+type Offset = Int
 
 data ConnectOpts = ConnectOpts {
-    _hostString :: String
-  , _topicName :: String
+    _hostString     :: String
+  , _topicName      :: String
   , _topicOverrides :: HK.ConfigOverrides
   , _kafkaOverrides :: HK.ConfigOverrides
   } deriving Show
@@ -30,13 +34,15 @@ kafkaSink
   :: (MonadSafe m)
   => ConnectOpts
   -> KafkaProducePartition
-  -> Consumer BS.ByteString (SafeT m) ()
-kafkaSink co partition = PS.bracket connect publish release
+  -> Consumer BS.ByteString (SafeT m) (Maybe HK.KafkaError)
+kafkaSink co partition = PS.bracket connect release publish
   where
-    connect = PS.liftBase $ do
+    connect = liftIO $ do
       kafka <- HK.newKafka HK.RdKafkaProducer (co ^. kafkaOverrides)
       HK.addBrokers kafka (co ^. hostString)
       topic <- HK.newKafkaTopic kafka (co ^. topicName) (co ^. topicOverrides)
       return (kafka, topic)
-    release (fst -> k) = PS.liftBase $ HK.drainOutQueue k
-    publish (k, t) = _ -- P.mapM (P.liftIO $ HK.produceMessage t partition . HK.KafkaProduceMessage)
+    release (fst -> k) = liftIO $ HK.drainOutQueue k
+    publish (snd -> t) = forever $ do
+      m <- P.await
+      P.liftIO $ HK.produceMessage t partition $ HK.KafkaProduceMessage m
