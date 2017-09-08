97d32cb8fdc86193938a10865dfb71d58bceb70b
Ben Ford <ben@perurbis.com>
2017-09-08 13:26:50 +0100

    Bring default.nix up to date

--- default.nix
+++ default.nix
@@ -1,13 +1,14 @@
-{ mkDerivation, base, bytestring, haskakafka, hedis, lens, mmorph
-, mtl, pipes, pipes-concurrency, pipes-safe, stdenv, transformers
+{ mkDerivation, base, bytestring, exceptions, hw-kafka-client
+, monad-logger, pipes, pipes-safe, stdenv, text, transformers
+, transformers-base
 }:
 mkDerivation {
   pname = "pipes-kafka";
   version = "0.1.0.0";
   src = ./.;
-  buildDepends = [
-    base bytestring haskakafka hedis lens mmorph mtl pipes
-    pipes-concurrency pipes-safe transformers
+  libraryHaskellDepends = [
+    base bytestring exceptions hw-kafka-client monad-logger pipes
+    pipes-safe text transformers transformers-base
   ];
   homepage = "https://github.com/boothead/pipes-kafka";
   description = "Kafka in the Pipes ecosystem";
